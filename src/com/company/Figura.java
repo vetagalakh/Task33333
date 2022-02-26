35b35d70aeeefa6ba9205229e62ca4082b31fd3a
vetagalakh <galakhvetta1@gmail.com>
2022-02-26 11:32:46 +0300

    home

---
+++ src/com/company/Figura.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Figura {
+    public void sq (int a, int b) {
+        int s = a * b;
+        System.out.println(s);
+    }
+}
+ 
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+Figura kvadrat = new Figura();
+    kvadrat.sq(3,3);
+
+    Figura pryam = new Figura();
+    pryam.sq(1, 5);
+    //System.out.println();
+    }
+}
